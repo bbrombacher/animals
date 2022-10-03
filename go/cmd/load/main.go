2ba2297b0b8f2a5fb1e5e5f11181348fc854c36a
bbrombacher <bbrombacher@me.com>
2022-10-02 17:23:03 -0700

    update main

--- go/cmd/load/main.go
+++ go/cmd/load/main.go
@@ -11,17 +11,21 @@ import (
 	"github.com/gammazero/workerpool"
 )
 
-var url = "http://localhost:8080/go-animals?limit=%v"
+var endpoint = "/go-animals?limit=%v"
+var baseLocalUrl = "http://localhost:8080"
+var baseServerUrl = "https://animals-production.up.railway.app"
 
 func main() {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
+	url := baseServerUrl
+
 	wp := workerpool.New(10)
 	for i := 0; i < 100; i++ {
 		wp.Submit(func() {
 			limit := r1.Intn(10000)
-			response := makeRequest(limit)
+			response := makeRequest(limit, url+endpoint)
 			if response != nil {
 				log.Println("response size: ", len(response["animals"].([]interface{})))
 			}
@@ -30,7 +34,7 @@ func main() {
 	wp.StopWait()
 }
 
-func makeRequest(limit int) map[string]interface{} {
+func makeRequest(limit int, url string) map[string]interface{} {
 
 	resp, err := http.DefaultClient.Get(fmt.Sprintf(url, limit))
 	if err != nil {
